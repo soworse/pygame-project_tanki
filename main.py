675ffb74da3a9459c635471b54a34e293fc720d6
pepe-hugo <hugesk07.14@gmail.com>
2023-01-12 20:02:21 +0300

    танк поворот с ошибкой

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+import os
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
+
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Hero(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super(Hero, self).__init__(*group)
+        self.image = load_image('tank.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.dig = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def update(self, vx, vy):
+        self.rect.x += vx
+        self.rect.y += vy
+
+    def turn(self, a):
+        self.image = pygame.transform.rotate(self.image, abs(a - self.dig))
+        self.dig = a
+        print(self.dig, '     ', a)
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+
+    all_sprites = pygame.sprite.Group()
+
+    hero = Hero(all_sprites)
+
+    running = True
+    pos_ = 0, 0
+    while running:
+        pygame.mouse.set_visible(0)
+        screen.fill('black')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    hero.update(-10, 0)
+                    hero.turn(0)
+                if event.key == pygame.K_RIGHT:
+                    hero.update(10, 0)
+                    hero.turn(180)
+                if event.key == pygame.K_UP:
+                    hero.update(0, -10)
+                    hero.turn(90)
+                if event.key == pygame.K_DOWN:
+                    hero.update(0, 10)
+                    hero.turn(270)
+        all_sprites.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+main()
