dbeda1d96c0b87026e7ab8960c1b87328918cb67
ЭРИК <merik1109@gmail.com>
2023-01-14 15:13:50 +0300

    поворот с отслеживанием прошлого положения

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import random
 
 pygame.init()
 arrow = pygame.image.load('Tank-GTA2.png')
-DEFAULT_IMAGE_SIZE = (50, 50)
-
+DEFAULT_IMAGE_SIZE = (150, 50)
+a = 0
+old_a = -1
 # Scale the image to your needed size
 arrow = pygame.transform.scale(arrow, DEFAULT_IMAGE_SIZE)
 screen = pygame.display.set_mode((500, 500))
@@ -15,18 +16,48 @@ x1, y1 = 0, 0
 while True:
     screen.fill((255, 255, 255))
     for event in pygame.event.get():
+        print(old_a)
+        print(a)    
         if event.type == pygame.KEYDOWN:
+            old_a = a
             if event.key == pygame.K_RIGHT:
                 x1 += 10
-                arrow = pygame.transform.rotate(arrow, 90)
+                a = 1
             if event.key == pygame.K_LEFT:
                 x1 -= 10
-                arrow = pygame.transform.rotate(arrow, 90)
+                a = 2
             if event.key == pygame.K_DOWN:
                 y1 += 10
-                arrow = pygame.transform.rotate(arrow, 90)
+                a = 3
             if event.key == pygame.K_UP:
+                a = 4
                 y1 -= 10
+
+        if old_a == 1 and a == 2:
+            arrow = pygame.transform.rotate(arrow, 180)
+        if old_a == 1 and a == 3:
+            arrow = pygame.transform.rotate(arrow, -90)
+        if old_a == 1 and a == 4:
+            arrow = pygame.transform.rotate(arrow, 90)
+        if old_a == 2 and a == 1:
+            arrow = pygame.transform.rotate(arrow, 180)
+        if old_a == 2 and a == 3:
+            arrow = pygame.transform.rotate(arrow, 90)
+        if old_a == 2 and a == 4:
+            arrow = pygame.transform.rotate(arrow, -90)
+        if old_a == 3 and a == 1:
+            arrow = pygame.transform.rotate(arrow, 90)
+        if old_a == 3 and a == 2:
+            arrow = pygame.transform.rotate(arrow, -90)
+        if old_a == 3 and a == 4:
+            arrow = pygame.transform.rotate(arrow, 180)
+        if old_a == 4 and a == 1:
+            arrow = pygame.transform.rotate(arrow, 90)
+        if old_a == 4 and a == 2:
+            arrow = pygame.transform.rotate(arrow, -90)
+        if old_a == 4 and a == 3:
+            arrow = pygame.transform.rotate(arrow, 180)
+
         screen.blit(arrow, (x1, y1))
         pygame.display.flip()
         if event.type == pygame.QUIT:
