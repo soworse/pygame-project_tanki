d133643cfa71f7eb87e4f1a4acd00a43fbb5189d
soworse <118914297+soworse@users.noreply.github.com>
2023-01-25 16:47:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,92 +1,108 @@
-import pygame
 import os
 import sys
-import random
+import pygame
+clock = pygame.time.Clock()
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 600, 600
 
-'''#cтартовый экран
 pygame.init()
-arrow = pygame.image.load('old-video-game-background-2451118.jpg')
-DEFAULT_IMAGE_SIZE = (500, 500)
-arrow = pygame.transform.scale(arrow, DEFAULT_IMAGE_SIZE)
-screen = pygame.display.set_mode((500, 500))
-while True:
-    screen.blit(arrow, (0, 0))
-    pygame.display.flip()
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
-            pygame.quit()
-            exit(0)
-    font = pygame.font.Font(None, 50)
-    text = font.render("FASHION TANKI", True, (255, 255, 255))
-    text1 = font.render("press any button to start", True, (255, 255, 255))
-    text_x = 500 // 2 - text.get_width() // 2
-    text_y = 500 // 5 - text.get_height() // 2
-    text_w = text.get_width()
-    text_h = text.get_height()
-    screen.blit(text, (text_x, text_y))
-    screen.blit(text1, (50, 300))
-    pygame.display.flip()'''
-#главный экран
-pygame.init()
-arrow = pygame.image.load('Tank-GTA2.png')
-DEFAULT_IMAGE_SIZE = (150, 50)
-a = 0
-old_a = -1
-# Scale the image to your needed size
-arrow = pygame.transform.scale(arrow, DEFAULT_IMAGE_SIZE)
-screen = pygame.display.set_mode((500, 500))
-flag = True
-x1, y1 = 0, 0
-while True:
-    screen.fill((255, 255, 255))
-    for event in pygame.event.get():
-        print(old_a)
-        print(a)    
-        if event.type == pygame.KEYDOWN:
-            old_a = a
-            if event.key == pygame.K_RIGHT:
-                x1 += 10
-                a = 1
-            if event.key == pygame.K_LEFT:
-                x1 -= 10
-                a = 2
-            if event.key == pygame.K_DOWN:
-                y1 += 10
-                a = 3
-            if event.key == pygame.K_UP:
-                a = 4
-                y1 -= 10
+size = width, height = 500, 500
+screen = pygame.display.set_mode(WINDOW_SIZE)
 
-        if old_a == 1 and a == 2:
-            arrow = pygame.transform.rotate(arrow, 180)
-        if old_a == 1 and a == 3:
-            arrow = pygame.transform.rotate(arrow, -90)
-        if old_a == 1 and a == 4:
-            arrow = pygame.transform.rotate(arrow, 90)
-        if old_a == 2 and a == 1:
-            arrow = pygame.transform.rotate(arrow, 180)
-        if old_a == 2 and a == 3:
-            arrow = pygame.transform.rotate(arrow, 90)
-        if old_a == 2 and a == 4:
-            arrow = pygame.transform.rotate(arrow, -90)
-        if old_a == 3 and a == 1:
-            arrow = pygame.transform.rotate(arrow, 90)
-        if old_a == 3 and a == 2:
-            arrow = pygame.transform.rotate(arrow, -90)
-        if old_a == 3 and a == 4:
-            arrow = pygame.transform.rotate(arrow, 180)
-        if old_a == 4 and a == 1:
-            arrow = pygame.transform.rotate(arrow, 90)
-        if old_a == 4 and a == 2:
-            arrow = pygame.transform.rotate(arrow, -90)
-        if old_a == 4 and a == 3:
-            arrow = pygame.transform.rotate(arrow, 180)
 
-        screen.blit(arrow, (x1, y1))
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+class F_screen(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super(F_screen
+              , self).__init__(*group)
+        self.image = load_image('old-video-game-background-2451118.jpg')
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+
+
+class Hero(pygame.sprite.Sprite):
+    def __init__(self, *group):
+        super(Hero, self).__init__(*group)
+        DEFAULT_IMAGE_SIZE = (100, 100)
+        self.image = load_image('tank.png')
+        self.image = pygame.transform.scale(self.image, DEFAULT_IMAGE_SIZE)
+        self.rect = self.image.get_rect()
+        self.rect.x = 0
+        self.rect.y = 0
+        self.dig = 0
+
+    def update(self, vx, vy):
+        self.rect.x += vx
+        self.rect.y += vy
+
+    def turn(self, a):
+        self.image = pygame.transform.rotate(self.image, abs(a - self.dig))
+        self.dig = a
+        print(self.dig, '     ', a)
+
+def start_screen():
+    intro_text = ["FASHION TANKI", 'press any button to start']
+    fon = pygame.transform.scale(load_image('old-video-game-background-2451118.jpg'), (700, 700))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 350
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
         pygame.display.flip()
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
+        clock.tick(50)
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    start_screen()
+    all_sprites = pygame.sprite.Group()
+    print(all_sprites)
+    hero = Hero(all_sprites)
+    running = True
+    pos_ = 0, 0
+    while running:
+        pygame.mouse.set_visible(0)
+        screen.fill('white')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    hero.update(-10, 0)
+                    hero.turn(0)
+                if event.key == pygame.K_RIGHT:
+                    hero.update(10, 0)
+                    hero.turn(180)
+                if event.key == pygame.K_UP:
+                    hero.update(0, -10)
+                    hero.turn(90)
+                if event.key == pygame.K_DOWN:
+                    hero.update(0, 10)
+                    hero.turn(270)
+        all_sprites.draw(screen)
+        pygame.display.flip()
+    pygame.quit()
+
+
+main()
